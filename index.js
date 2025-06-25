b5e6459095833ff2e1bdc491e2442a0497a7db03
Tama10-10 <tamadebnath2001@gmail.com>
2025-06-25 23:08:34 +0600

    Use dynamic PORT for Render

--- index.js
+++ index.js
@@ -180,7 +180,8 @@ socket.on("user-logout", (userId) => {
 
 
 
-// ✅ Start server
-server.listen(3000, () => {
-  console.log('Server running at http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
